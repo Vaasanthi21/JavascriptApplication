efc3e6a140ac382b61fec589f54037331746a84e
Vaasanthi21 <167667051+Vaasanthi21@users.noreply.github.com>
2025-12-04 11:36:15 +0530

    Update todos.js

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,1 +1,12 @@
+import { displayTodos } from './displayTodos.js';
+
+if (localStorage.getItem('isLoggedIn') !== 'true') {
+  window.location.href = 'login.html';
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('todoContainer').innerHTML = displayTodos(data);
+  });
 
