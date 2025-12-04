e796d4b95500e5f2cb0183a4c33443f85696e837
Vaasanthi21 <167667051+Vaasanthi21@users.noreply.github.com>
2025-12-04 11:35:33 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,1 +1,14 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const email = e.target.email.value;
+  const password = e.target.password.value;
+
+  if (storedUser && storedUser.email === email && storedUser.password === password) {
+    localStorage.setItem('isLoggedIn', 'true');
+    window.location.href = 'todos.html';
+  } else {
+    alert('Invalid credentials');
+  }
+});
 
